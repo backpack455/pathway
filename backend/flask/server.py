5d4132f8126876415b06e32be626eb6944e86285
Cucklecumber <25mx.free@gmial.com>
2022-08-03 22:51:48 -0400

    Big Brain Max and his AI ML

---
+++ backend/flask/server.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_restful import Api, Resource, abort, reqparse
+from flask_cors import CORS
+from therapy_diagnoser import therapy_diagnoser
+import tensorflow as tf
+
+#physical_devices = tf.config.list_physical_devices('GPU') 
+#for device in physical_devices:
+#    tf.config.experimental.set_memory_growth(device, True)
+    
+    
+#FLASK SETUP
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+
+#THERAPY DIAGNOSER
+therapy_diagnoser_get_args = reqparse.RequestParser()
+therapy_diagnoser_get_args.add_argument("user_input", help="Please Add Valid Input", required=True, location='form')
+
+my_therapy_diagnoser = therapy_diagnoser() 
+
+class therapy_diagnoser(Resource): 
+    def get(self):
+        
+        user_input = therapy_diagnoser_get_args.parse_args()
+        category = my_therapy_diagnoser.categorize_problem(user_input)
+        sentiment = my_therapy_diagnoser.predict_sentiment(user_input['user_input'])
+      
+        return {"category diagnosis": f"{category}", "sentiment_analysis": f"{sentiment}"}
+    
+
+
+api.add_resource(therapy_diagnoser, '/diagnoser')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3001)
